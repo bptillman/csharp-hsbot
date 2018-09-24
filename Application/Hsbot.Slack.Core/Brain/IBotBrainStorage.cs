7ef284ae02a223a4ba0324fa06408542ff674ac0
Dean Goodman <dean.goodman@headspring.com>
2018-09-23 22:56:20 -0500

    Make brain storage async

--- Application/Hsbot.Slack.Core/Brain/IBotBrainStorage.cs
+++ Application/Hsbot.Slack.Core/Brain/IBotBrainStorage.cs
@@ -1,9 +1,11 @@
-﻿namespace Hsbot.Slack.Core.Brain
+﻿using System.Threading.Tasks;
+
+namespace Hsbot.Slack.Core.Brain
 {
     public interface IBotBrainStorage<T>
         where T : IBotBrain
     {
-        T Load(string address);
-        void Save(string address, T brain);
+        Task<T> Load();
+        Task Save(T brain);
     }
 }
