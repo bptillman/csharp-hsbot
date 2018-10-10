27ca4f7d5c50979bad28f5d7c197458136310b8b
Dean Goodman <dean.goodman@headspring.com>
2018-10-09 21:11:02 -0500

    Whitespace fix

--- Application/Hsbot.Slack.Web/HsbotServiceExtensions.cs
+++ Application/Hsbot.Slack.Web/HsbotServiceExtensions.cs
@@ -9,29 +9,29 @@ namespace Hsbot.Slack.Web
 {
     public static class HsbotServiceExtensions
     {
-      public static IServiceCollection AddHsbot(this IServiceCollection services, HsbotConfig config)
-      {
-        RegisterMessageHandlers(services);
-
-        services.AddSingleton<IHsbotLog, HsbotLog>();
-        services.AddSingleton<IHsbotConfig>(svc => config);
-        services.AddSingleton<IRandomNumberGenerator, RandomNumberGenerator>();
-        services.AddSingleton<Core.Hsbot>();
-
-        return services;
-      }
-
-      private static void RegisterMessageHandlers(IServiceCollection services)
-      {
-        var handlerInterfaceType = typeof(IInboundMessageHandler);
-        var messageHandlerTypes = Assembly.GetAssembly(typeof(Core.Hsbot))
-          .GetTypes()
-          .Where(t => !t.IsAbstract && !t.IsInterface && handlerInterfaceType.IsAssignableFrom(t));
-
-        foreach (var messageHandlerType in messageHandlerTypes)
+        public static IServiceCollection AddHsbot(this IServiceCollection services, HsbotConfig config)
         {
-          services.Add(new ServiceDescriptor(handlerInterfaceType, messageHandlerType, ServiceLifetime.Transient));
+            RegisterMessageHandlers(services);
+
+            services.AddSingleton<IHsbotLog, HsbotLog>();
+            services.AddSingleton<IHsbotConfig>(svc => config);
+            services.AddSingleton<IRandomNumberGenerator, RandomNumberGenerator>();
+            services.AddSingleton<Core.Hsbot>();
+
+            return services;
         }
-      }
+
+        private static void RegisterMessageHandlers(IServiceCollection services)
+        {
+            var handlerInterfaceType = typeof(IInboundMessageHandler);
+            var messageHandlerTypes = Assembly.GetAssembly(typeof(Core.Hsbot))
+              .GetTypes()
+              .Where(t => !t.IsAbstract && !t.IsInterface && handlerInterfaceType.IsAssignableFrom(t));
+
+            foreach (var messageHandlerType in messageHandlerTypes)
+            {
+                services.Add(new ServiceDescriptor(handlerInterfaceType, messageHandlerType, ServiceLifetime.Transient));
+            }
+        }
     }
 }
--- Application/Hsbot.Slack.Web/Startup.cs
+++ Application/Hsbot.Slack.Web/Startup.cs
@@ -11,23 +11,23 @@ namespace Hsbot.Slack.Web
 {
     public class Startup
     {
-      private readonly IConfiguration _config;
+        private readonly IConfiguration _config;
 
-      public Startup(IConfiguration config)
-      {
-        _config = config;
-      }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
-      public void ConfigureServices(IServiceCollection services)
-      {
-        services.AddLogging();
-        services.AddHsbot(LoadConfig());
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddHsbot(LoadConfig());
 
-        //This registration is what will actually run hsbot as a background
-        //process within the website.  We'll need an external keep-alive to
-        //make sure the site doesn't shut down when no HTTP requests are coming in.
-        services.AddSingleton<IHostedService, HsbotHostedService>();
-      }
+            //This registration is what will actually run hsbot as a background
+            //process within the website.  We'll need an external keep-alive to
+            //make sure the site doesn't shut down when no HTTP requests are coming in.
+            services.AddSingleton<IHostedService, HsbotHostedService>();
+        }
 
         private HsbotConfig LoadConfig()
         {
@@ -41,19 +41,19 @@ namespace Hsbot.Slack.Web
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-      {
-        if (env.IsDevelopment())
         {
-            app.UseDeveloperExceptionPage();
-        }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-        //We're not actually serving up any web content at present.
-        //This website is only acting as a host for the hsbot service,
-        //so no need to wire up anything other than a static reply.
-        app.Run(async (context) =>
-        {
-            await context.Response.WriteAsync("Beep boop bop");
-        });
-      }
+            //We're not actually serving up any web content at present.
+            //This website is only acting as a host for the hsbot service,
+            //so no need to wire up anything other than a static reply.
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Beep boop bop");
+            });
+        }
     }
 }
