489758865403a1f94d0d94bbb4a452298139a927
Dean Goodman <dean.goodman@headspring.com>
2018-10-22 19:47:20 -0500

    Add tests for MessageHandlerBase

--- Application/Hsbot.Slack.Core.Tests/HsbotTests.cs
+++ Application/Hsbot.Slack.Core.Tests/HsbotTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using Hsbot.Slack.Core.Brain;
 using Hsbot.Slack.Core.Connection;
 using Hsbot.Slack.Core.Messaging;
 using Moq;
+using Shouldly;
 
 namespace Hsbot.Slack.Core.Tests
 {
@@ -79,9 +81,8 @@ namespace Hsbot.Slack.Core.Tests
             var chatConnectorMock = MockChatConnector();
             chatConnectorMock.Setup(x => x.MessageReceived).Returns(Observable.Return(Task.FromResult(inboundMessage)));
 
-            var outboundResponse = new OutboundResponse();
             var messageHandlerMock = new Mock<IInboundMessageHandler>();
-            messageHandlerMock.Setup(x => x.Handle(It.IsAny<InboundMessage>())).Returns(new[] {outboundResponse});
+            messageHandlerMock.Setup(x => x.HandleAsync(It.IsAny<BotMessageContext>())).Returns(Task.CompletedTask);
             messageHandlerMock.Setup(x => x.Handles(It.IsAny<InboundMessage>()))
                 .Returns(new HandlesResult {HandlesMessage = true});
 
@@ -89,8 +90,46 @@ namespace Hsbot.Slack.Core.Tests
             await hsbot.Connect();
 
             messageHandlerMock.Verify(x => x.Handles(inboundMessage), Times.Once);
-            messageHandlerMock.Verify(x => x.Handle(inboundMessage), Times.Once);
-            chatConnectorMock.Verify(x => x.SendMessage(outboundResponse), Times.Once);
+            messageHandlerMock.Verify(x => x.HandleAsync(It.IsAny<BotMessageContext>()), Times.Once);
+        }
+
+        public async Task ShouldNotCallHandlerWhenItDoesNotHandleMessage()
+        {
+            var logMock = MockLog();
+            var brainStorageMock = MockBrainStorage();
+
+            var inboundMessage = new InboundMessage
+            {
+                BotIsMentioned = true,
+                BotId = "test",
+                BotName = "test",
+                Channel = "fake channel",
+                ChannelName = "fake channel",
+                FullText = "test message",
+                MessageRecipientType = MessageRecipientType.Channel,
+                RawText = "test message",
+                TextWithoutBotName = "test message",
+                UserChannel = "",
+                UserEmail = "test@test.com",
+                UserId = "nobody",
+                Username = "nobody"
+            };
+
+            var chatConnectorMock = MockChatConnector();
+            chatConnectorMock.Setup(x => x.MessageReceived).Returns(Observable.Return(Task.FromResult(inboundMessage)));
+
+            var outboundResponse = new OutboundResponse();
+            var messageHandlerMock = new Mock<IInboundMessageHandler>();
+            messageHandlerMock.Setup(x => x.HandleAsync(It.IsAny<BotMessageContext>())).Returns(Task.CompletedTask);
+            messageHandlerMock.Setup(x => x.Handles(It.IsAny<InboundMessage>()))
+                .Returns(new HandlesResult {HandlesMessage = false});
+
+            var hsbot = new Hsbot(logMock.Object, new []{ messageHandlerMock.Object }, brainStorageMock.Object, chatConnectorMock.Object);
+            await hsbot.Connect();
+
+            messageHandlerMock.Verify(x => x.Handles(inboundMessage), Times.Once);
+            messageHandlerMock.Verify(x => x.HandleAsync(It.IsAny<BotMessageContext>()), Times.Never);
+            chatConnectorMock.Verify(x => x.SendMessage(outboundResponse), Times.Never);
         }
 
         public async Task ShouldSendMessageResponseViaChatConnector()
