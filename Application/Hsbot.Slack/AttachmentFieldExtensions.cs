73b9826ef936523eed48bcb4ffc8ab15206b54e2
Dean Goodman <dean.goodman@headspring.com>
2018-10-26 21:36:34 -0500

    Add new library for slack-specific classes

---
+++ Application/Hsbot.Slack/AttachmentFieldExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Hsbot.Core.Messaging;
+using SlackConnector.Models;
+
+namespace Hsbot.Slack
+{
+    internal static class AttachmentFieldExtensions
+    {
+        public static IList<SlackAttachmentField> ToSlackAttachmentFields(this List<AttachmentField> attachmentFields)
+        {
+            var result = new List<SlackAttachmentField>();
+
+            if (attachmentFields == null) return result;
+
+            foreach (var attachmentField in attachmentFields)
+            {
+                result.Add(new SlackAttachmentField
+                {
+                    Title = attachmentField.Title,
+                    Value = attachmentField.Value,
+                    IsShort = attachmentField.IsShort
+                });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Application/Hsbot.Slack/SlackMessageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SlackConnector.Models;
+
+namespace Hsbot.Slack
+{
+    internal static class SlackMessageExtensions
+    {
+        public static string GetTextWithoutBotName(this SlackMessage incomingMessage, IEnumerable<string> botNames)
+        {
+            var messageText = incomingMessage.Text ?? string.Empty;
+            var handle = botNames.FirstOrDefault(x => messageText.StartsWith(x, StringComparison.OrdinalIgnoreCase));
+
+            return string.IsNullOrEmpty(handle) ? messageText : messageText.Substring(handle.Length).Trim();
+        }
+    }
+}
