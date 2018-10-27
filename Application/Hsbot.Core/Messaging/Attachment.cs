a30df911a0469a3831fe97511f455f1eb8455eb4
Dean Goodman <dean.goodman@headspring.com>
2018-10-26 21:37:40 -0500

    Remove slack-specific classes from core

--- Application/Hsbot.Core/Messaging/Attachment.cs
+++ Application/Hsbot.Core/Messaging/Attachment.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using SlackConnector.Models;
 
 namespace Hsbot.Core.Messaging
 {
@@ -37,33 +36,6 @@ namespace Hsbot.Core.Messaging
             });
 
             return this;
-        }
-    }
-
-    internal static class AttachmentExtensions
-    {
-        public static IList<SlackAttachment> ToSlackAttachments(this List<Attachment> attachments)
-        {
-            var slackAttachments = new List<SlackAttachment>();
-
-            if (attachments == null) return slackAttachments;
-
-            foreach (var attachment in attachments)
-            {
-                slackAttachments.Add(new SlackAttachment
-                {
-                    Text = attachment.Text,
-                    Title = attachment.Title,
-                    Fallback = attachment.Fallback,
-                    ImageUrl = attachment.ImageUrl,
-                    ThumbUrl = attachment.ThumbUrl,
-                    AuthorName = attachment.AuthorName,
-                    ColorHex = attachment.Color,
-                    Fields = attachment.AttachmentFields.ToSlackAttachmentFields()
-                });
-            }
-
-            return slackAttachments;
         }
     }
 }
--- Application/Hsbot.Core/Messaging/AttachmentField.cs
+++ Application/Hsbot.Core/Messaging/AttachmentField.cs
@@ -1,34 +1,9 @@
-﻿using System.Collections.Generic;
-using SlackConnector.Models;
-
-namespace Hsbot.Core.Messaging
+﻿namespace Hsbot.Core.Messaging
 {
     public class AttachmentField
     {
         public string Title { get; set; }
         public string Value { get; set; }
         public bool IsShort { get; set; }
-    }
-
-    internal static class AttachmentFieldExtensions
-    {
-        public static IList<SlackAttachmentField> ToSlackAttachmentFields(this List<AttachmentField> attachmentFields)
-        {
-            var result = new List<SlackAttachmentField>();
-
-            if (attachmentFields == null) return result;
-
-            foreach (var attachmentField in attachmentFields)
-            {
-                result.Add(new SlackAttachmentField
-                {
-                    Title = attachmentField.Title,
-                    Value = attachmentField.Value,
-                    IsShort = attachmentField.IsShort
-                });
-            }
-
-            return result;
-        }
     }
 }
