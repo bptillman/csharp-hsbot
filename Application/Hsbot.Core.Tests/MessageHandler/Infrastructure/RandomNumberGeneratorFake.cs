9a22d70b6d2409536397c08dceae11e85faa5eff
Bill Ross <bill.ross@headspring.com>
2018-11-01 18:59:47 -0500

    Added fake for new generate method.

--- Application/Hsbot.Core.Tests/MessageHandler/Infrastructure/RandomNumberGeneratorFake.cs
+++ Application/Hsbot.Core.Tests/MessageHandler/Infrastructure/RandomNumberGeneratorFake.cs
@@ -5,10 +5,16 @@ namespace Hsbot.Core.Tests.MessageHandler.Infrastructure
     public class RandomNumberGeneratorFake : IRandomNumberGenerator
     {
         public double NextDoubleValue { get; set; }
+        public int NextIntValue { get; set; }
 
         public double Generate()
         {
             return NextDoubleValue;
+        }
+
+        public int Generate(int minValue, int maxValue)
+        {
+            return NextIntValue;
         }
     }
 }
