ee51317474a88236131cb6fd671fa5210117723d
Allison O'Hanlon <allison.ohanlon@headspring.com>
2018-11-01 20:32:15 -0500

    Moved arrray outside of the function

--- Application/Hsbot.Core/MessageHandlers/Magic8MessageHandler.cs
+++ Application/Hsbot.Core/MessageHandlers/Magic8MessageHandler.cs
@@ -24,33 +24,33 @@ namespace Hsbot.Core.MessageHandlers
             yield break;
         }
 
+        public static readonly string[] Answer = {
+            "It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Do not count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+        };
+
         public override Task HandleAsync(IBotMessageContext context)
         {
-            var answer = new string[]
-            {
-                "It is certain",
-                "It is decidedly so",
-                "Without a doubt",
-                "Yes definitely",
-                "You may rely on it",
-                "As I see it, yes",
-                "Most likely",
-                "Outlook good",
-                "Yes",
-                "Signs point to yes",
-                "Reply hazy try again",
-                "Ask again later",
-                "Better not tell you now",
-                "Cannot predict now",
-                "Concentrate and ask again",
-                "Do not count on it",
-                "My reply is no",
-                "My sources say no",
-                "Outlook not so good",
-                "Very doubtful"
-            };
-            var random = RandomNumberGenerator.Generate(0, answer.Length);
-            return ReplyToChannel(context, answer[random]);
+            var random = RandomNumberGenerator.Generate(0, Answer.Length);
+            return ReplyToChannel(context, Answer[random]);
         }
 
         protected override bool CanHandle(InboundMessage message)
