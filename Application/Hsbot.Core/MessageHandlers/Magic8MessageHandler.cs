82b46ce9924909c26c3d1db946b749e0b472d070
Allison O'Hanlon <allison.ohanlon@headspring.com>
2018-11-01 19:31:52 -0500

    Magic 8 Message Handler created

---
+++ Application/Hsbot.Core/MessageHandlers/Magic8MessageHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hsbot.Core.Messaging;
+using Hsbot.Core.Random;
+
+namespace Hsbot.Core.MessageHandlers
+{
+    public class Magic8MessageHandler : MessageHandlerBase
+    {
+        public Magic8MessageHandler(IRandomNumberGenerator randomNumberGenerator) : base(randomNumberGenerator)
+        {
+        }
+
+        public override string[] TargetedChannels => AllChannels;
+        public override bool DirectMentionOnly => false;
+
+        public override double GetHandlerOdds(InboundMessage message)
+        {
+            return 0.1;
+        }
+
+        public override IEnumerable<MessageHandlerDescriptor> GetCommandDescriptors()
+        {
+            yield break;
+        }
+
+        public override Task HandleAsync(IBotMessageContext context)
+        {
+            var answer = new string[]
+            {
+                "It is certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Do not count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful"
+            };
+            var random = RandomNumberGenerator.Generate(0, answer.Length);
+            return ReplyToChannel(context, answer[random]);
+        }
+
+        protected override bool CanHandle(InboundMessage message)
+        {
+            return message.Contains("will") || message.Contains("Will") || message.Contains("?");
+        }
+    }
+}
