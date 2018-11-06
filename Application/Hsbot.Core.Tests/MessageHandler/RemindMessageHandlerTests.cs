226d746296b96de41987faa03fbc9f5c08c9addd
Dean Goodman <dean.goodman@headspring.com>
2018-11-05 22:11:13 -0600

    Add remind message handler

---
+++ Application/Hsbot.Core.Tests/MessageHandler/RemindMessageHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hsbot.Core.MessageHandlers;
+using Hsbot.Core.Tests.MessageHandler.Infrastructure;
+
+namespace Hsbot.Core.Tests.MessageHandler
+{
+    public class RemindMessageHandlerTests : MessageHandlerTestBase<RemindMessageHandler>
+    {
+        protected override string[] MessageTextsThatShouldBeHandled => new[] {"remind me in 1 week to test", "remind me in 1 day to test", "remind me in 1 hour to test", "remind me in 1 minute to test"};
+        protected override string[] MessageTextsThatShouldNotBeHandled => new[] {"remind me", "remind me to test", "remind me in 1 foo to test", "remind me in 1 hour"};
+    }
+}
