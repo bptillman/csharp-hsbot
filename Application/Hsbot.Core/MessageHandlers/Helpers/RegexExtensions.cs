aad5ddb082313ddcdbd5140bb00542c35283ccfd
Dean Goodman <dean.goodman@headspring.com>
2020-04-16 22:05:49 -0500

    Refactor regex capture helper to extension method

---
+++ Application/Hsbot.Core/MessageHandlers/Helpers/RegexExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Hsbot.Core.MessageHandlers.Helpers
+{
+    internal static class RegexExtensions
+    {
+        public static int? CaptureToInt(this Capture capture)
+        {
+            return capture == null || string.IsNullOrEmpty(capture.Value)
+                ? (int?)null
+                : int.Parse(capture.Value);
+        }
+    }
+}
