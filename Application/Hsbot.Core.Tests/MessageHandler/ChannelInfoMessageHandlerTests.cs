2f50c31da92db5dfbcf3eaa4c1f3d6e2179a6f21
Dane Schilling <dane.schilling@headspring.com>
2020-04-23 11:43:16 -0500

    Add admin channel info handler to get channel info for testing and debugging.

---
+++ Application/Hsbot.Core.Tests/MessageHandler/ChannelInfoMessageHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Hsbot.Core.MessageHandlers;
+using Hsbot.Core.Tests.MessageHandler.Infrastructure;
+using Shouldly;
+
+namespace Hsbot.Core.Tests.MessageHandler
+{
+    public class ChannelInfoMessageHandlerTests : MessageHandlerTestBase<ChannelInfoMessageHandler>
+    {
+        protected override string[] MessageTextsThatShouldBeHandled => new [] {"admin channel info"};
+        protected override string[] MessageTextsThatShouldNotBeHandled => new[] {"channel info"};
+
+        public async Task ShouldSayChannelInfo()
+        {
+            var messageHandler = GetHandlerInstance();
+            var response = await messageHandler.TestHandleAsync();
+            response.SentMessages.Count.ShouldBe(1);
+            response.SentMessages.First().Text.ShouldBe("Channel: Fake channel\nChannel name: Fake channel");
+        }
+    }
+}
---
+++ Application/Hsbot.Core/MessageHandlers/ChannelInfoMessageHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hsbot.Core.Messaging;
+using Hsbot.Core.Random;
+
+namespace Hsbot.Core.MessageHandlers
+{
+    public class ChannelInfoMessageHandler : MessageHandlerBase
+    {
+        public ChannelInfoMessageHandler(IRandomNumberGenerator randomNumberGenerator) : base(randomNumberGenerator)
+        {
+        }
+
+        public override IEnumerable<MessageHandlerDescriptor> GetCommandDescriptors()
+        {
+            return new List<MessageHandlerDescriptor>();
+        }
+
+        protected override bool CanHandle(InboundMessage message)
+        {
+            return message.StartsWith("admin channel info");
+        }
+
+        public override async Task HandleAsync(IInboundMessageContext context)
+        {
+            var message = context.Message;
+            await context.SendMessage(message.CreateResponse(
+                $"Channel: {message.Channel}\nChannel name: {message.ChannelName}"));
+        }
+    }
+}
