2a7ba91a0abc45157c54774934a1b9fb165571d4
Dean Goodman <dean.goodman@headspring.com>
2020-04-30 17:43:09 -0500

    Create SignalR chat hub and client

---
+++ Application/Hsbot.Blazor.Client/ChatClient.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Hsbot.Blazor.Client
+{
+    public class ChatClient : IAsyncDisposable
+    {
+        public const string HubUrl = "/LocalDebug/Chat";
+        public const string SendMessageMethodName = "SendMessage";
+        public const string ReceiveMessageMethodName = "ReceiveMessage";
+
+        private bool _started = false;
+
+        private readonly string _hubUrl;
+        private HubConnection _hubConnection;
+        
+        public ChatClient(string serverUrl)
+        {
+            _hubUrl = serverUrl.TrimEnd('/') + HubUrl;
+        }
+
+        public async Task StartAsync()
+        {
+            
+            if (!_started)
+            {
+                _hubConnection = new HubConnectionBuilder()
+                    .WithUrl(_hubUrl)
+                    .Build();
+
+                Console.WriteLine("ChatClient: calling Start()");
+
+                _hubConnection.On<string, string, string, bool>(ReceiveMessageMethodName, HandleReceiveMessage);
+
+                await _hubConnection.StartAsync();
+
+                Console.WriteLine("ChatClient: Start returned");
+                _started = true;
+            }
+        }
+
+        public async Task StopAsync()
+        {
+            if (_started)
+            {
+                await _hubConnection.StopAsync();
+                await _hubConnection.DisposeAsync();
+                _hubConnection = null;
+                _started = false;
+            }
+        }
+
+        public async Task SendAsync(string channelName, string userName, string messageText)
+        {
+            if (!_started)
+                throw new InvalidOperationException("Client not started");
+
+            await _hubConnection.SendAsync(SendMessageMethodName, channelName, userName, messageText);
+        }
+        
+        public async ValueTask DisposeAsync()
+        {
+            Console.WriteLine("ChatClient: Disposing");
+            await StopAsync();
+        }
+
+        public event MessageReceivedEventHandler MessageReceived;
+        public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+
+        private void HandleReceiveMessage(string channelName, string userName, string messageText, bool indicateTyping)
+        {
+            var messageReceivedEvent = new MessageReceivedEventArgs
+            {
+                ChannelName = channelName,
+                IndicateTyping = indicateTyping,
+                MessageText = messageText,
+                UserName = userName
+            };
+
+            MessageReceived?.Invoke(this, messageReceivedEvent);
+        }
+
+        public class MessageReceivedEventArgs : EventArgs
+        {
+            public string ChannelName { get; set; }
+            public string UserName { get; set; }
+            public string MessageText { get; set; }
+            public bool IndicateTyping { get; set; }
+        }
+    }
+}
---
+++ Application/Hsbot.Hosting.Web/LocalDebug/Hubs/ChatHub.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Hsbot.Core.Messaging;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Hsbot.Hosting.Web.LocalDebug.Hubs
+{
+    public sealed class ChatHub : Hub
+    {
+        private readonly DebugChatConnector _debugChatConnector;
+        public const string ReceiveMethodName = "ReceiveMessage";
+
+        public ChatHub(DebugChatConnector debugChatConnector)
+        {
+            _debugChatConnector = debugChatConnector;
+        }
+
+        public Task SendMessage(string channelName, string userName, string messageText)
+        {
+            messageText = messageText.Trim();
+            var message = new InboundMessage
+            {
+                RawText = messageText,
+                FullText = messageText,
+                TextWithoutBotName = GetTextWithoutBotName(messageText),
+                UserId = userName,
+                Username = userName,
+                UserEmail = $"{userName}@foo.com",
+                UserChannel = userName,
+                Channel = channelName,
+                ChannelName = channelName,
+                MessageRecipientType = MessageRecipientType.Channel,
+                BotName = "hsbot",
+                BotId = "@hsbot",
+                BotIsMentioned = BotIsMentioned(messageText)
+            };
+
+            _debugChatConnector.ReceiveMessage(message);
+            return Task.CompletedTask;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Connected to chat hub");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+
+        private bool BotIsMentioned(string messageText)
+        {
+            return _debugChatConnector.BotAliases.Any(n => messageText.StartsWith(n));
+        }
+
+        private string GetTextWithoutBotName(string messageText)
+        {
+            var handle = _debugChatConnector.BotAliases.FirstOrDefault(x => messageText.StartsWith(x, StringComparison.OrdinalIgnoreCase));
+            return string.IsNullOrEmpty(handle) ? messageText : messageText.Substring(handle.Length).Trim();
+        }
+    }
+}
