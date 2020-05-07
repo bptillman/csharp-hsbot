e02a14161632b4fdeb3ca7177a52e1d07e6cbb61
Ben Tillman <ben@headspring.com>
2020-05-07 17:17:24 -0500

    Start pug handler and test

---
+++ Application/Hsbot.Core.Tests/MessageHandler/PugBombMessageHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using Hsbot.Core.MessageHandlers;
+using Hsbot.Core.Tests.MessageHandler.Infrastructure;
+
+namespace Hsbot.Core.Tests.MessageHandler
+{
+    public class PugBombMessageHandlerTests : MessageHandlerTestBase<PugBombMessageHandler>
+    {
+        protected override string[] MessageTextsThatShouldBeHandled => new[] {"pug me", "pug bomb", "pug bomb 4"};
+        protected override string[] MessageTextsThatShouldNotBeHandled => new[] {"pug"};
+
+    }
+}
---
+++ Application/Hsbot.Core/MessageHandlers/PugBombMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hsbot.Core.ApiClients;
+using Hsbot.Core.Messaging;
+using Hsbot.Core.Random;
+
+namespace Hsbot.Core.MessageHandlers
+{
+    public class PugBombMessageHandler : MessageHandlerBase
+    {
+        private readonly IPugClient _pugClient;
+
+        public PugBombMessageHandler(IRandomNumberGenerator randomNumberGenerator, IPugClient pugClient) : base(randomNumberGenerator)
+        {
+            _pugClient = pugClient;
+        }
+
+        public override IEnumerable<MessageHandlerDescriptor> GetCommandDescriptors()
+        {
+            return new List<MessageHandlerDescriptor>()
+            {
+                new MessageHandlerDescriptor { Command = "pug me", Description = "Receive a pug"},
+                new MessageHandlerDescriptor { Command = "pug bomb <N>", Description = "Get N pugs"}
+            };
+        }
+
+        protected override bool CanHandle(InboundMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task HandleAsync(IInboundMessageContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
